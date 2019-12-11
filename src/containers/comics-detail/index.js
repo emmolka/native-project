13221f773acdda280a8d9a491dba6cce687b3bd7
Master <git@github.com:emmolka/NavBar.git>
2019-12-11 17:43:25 +0100

    push

--- src/containers/comics-detail/index.js
+++ src/containers/comics-detail/index.js
@@ -1,15 +1,15 @@
-import React from "react";
-import { MainWrapper, Image } from "./styles";
-import { Text } from "react-native";
+import React from 'react';
+import { MainWrapper, Image } from './styles';
+import { Text } from 'react-native';
 
-const ComicsDetails = props => {
-  const image = props.navigation.getParam("img");
+const ComicsDetails = (props) => {
+	const image = props.navigation.getParam('img');
 
-  return (
-    <MainWrapper>
-      <Image source={{ uri: image }} resizeMode="contain" />
-    </MainWrapper>
-  );
+	return (
+		<MainWrapper>
+			<Image source={{ uri: image }} resizeMode="contain" />
+		</MainWrapper>
+	);
 };
 
 export default ComicsDetails;
